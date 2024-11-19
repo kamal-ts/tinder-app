d32afc7cd1231fcd0ad63a18e66a1558f691f7bd
kamal1234567890 <kamaluddin1623@gmail.com>
2024-11-19 23:58:49 +0900

    change rendFIle

--- api/server.js
+++ api/server.js
@@ -47,7 +47,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "/client/dist")));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+        res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
     });
 }
 
