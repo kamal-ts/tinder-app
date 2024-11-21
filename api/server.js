f218f5b413f8e7a06b2e6c4ec12e9099a86dae5c
kamal1234567890 <kamaluddin1623@gmail.com>
2024-11-21 16:50:07 +0900

    add get port

--- api/server.js
+++ api/server.js
@@ -36,6 +36,9 @@ app.use(cors({
     credentials: true
 }))
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/matches", matchRoutes);
